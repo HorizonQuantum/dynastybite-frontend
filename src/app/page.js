56252cb76c1e83abe85d504e4e42c780ae8b5e02
Horizon <horizonquantum03@gmail.com>
2025-07-13 12:57:53 +0700

    init update

--- src/app/page.js
+++ src/app/page.js
@@ -52,9 +52,9 @@ export default function Home() {
                 </div>
                 <div className="w-[380px] text-[12px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ratione assumenda soluta explicabo aut? Repudiandae reprehenderit blanditiis quasi animi autem quos. Suscipit amet dolorum vel.</div>
                 <div className="w-[200px]">
-                  <a href="./auth/login" className="flex justify-center w-full py-[8px] font-bold text-[18px] rounded-lg text-white" style={{backgroundColor:`${color}`, boxShadow:"10px 10px 30px #FFE8B7, -10px -10px 30px #FFFBF4"}}>
+                  <Link href="./auth/login" className="flex justify-center w-full py-[8px] font-bold text-[18px] rounded-lg text-white" style={{backgroundColor:`${color}`, boxShadow:"10px 10px 30px #FFE8B7, -10px -10px 30px #FFFBF4"}}>
                       Pesan Sekarang
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="flex justify-end">
